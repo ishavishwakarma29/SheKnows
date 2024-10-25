4b57e6d889f879ba3561c08e2ebc611dce3e21cb
ishavishwakarma29 <ishavishwakarma29@gmail.com>
2024-10-26 01:54:02 +0530

    flask_model_integration

--- app.py
+++ app.py
@@ -1,33 +1,63 @@
 import flask
 from werkzeug.utils import secure_filename
 import os
-# start flask
+import numpy as np
+from PIL import Image
+import pickle
+
 app = flask.Flask(__name__, template_folder='./templates', static_folder='./static')
 
-app.config['UPLOAD_FOLDER'] = 'uploads/'  # Define a folder to save uploaded files
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit upload size to 16MB
+app.config['UPLOAD_FOLDER'] = './uploads/' 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
 
-# Ensure upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+model = pickle.load(open("G:/SheKnows/SheKnows/models/breast.sav", "rb"))
 
-# render default webpage
+
 @app.route('/', methods=['GET', 'POST']) 
 def index(): 
     if flask.request.method == 'POST':
-        if 'image' not in flask.request.files:
-            return "No file part in the request"
-        
         file = flask.request.files['image']
         
         if file.filename == '':
             return "No selected file"
-        
+
         if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return f"Image {filename} uploaded successfully!"
+           filename = secure_filename(file.filename)
+           filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+           file.save(filepath)
+           prediction = make_prediction(filepath)
+           if prediction==1:
+               return flask.redirect('positive')
+           else :
+               return flask.redirect('negative')
+               
 
     return flask.render_template('index.html') 
  
+@app.route('/positive', methods=['GET', 'POST']) 
+def rspos(): 
+    return flask.render_template('positive.html')
+
+@app.route('/negative', methods=['GET', 'POST']) 
+def rsneg(): 
+    return flask.render_template('negative.html')
+
+
+def make_prediction(image_path):
+    img_array = preprocess_image(image_path, target_size=(50, 50))
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    return int(predicted_class)
+
+def preprocess_image(image_path, target_size=(50, 50)):
+    img = Image.open(image_path)
+    img = img.resize(target_size)
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    return img_array
+
 if __name__ == '__main__': 
     app.run(debug=True) 
